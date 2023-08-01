bf61ea8d5e368c532e27b92593e846864dcb5392
Smart Top Dev <96333955+Smart-Top-Dev007@users.noreply.github.com>
2023-08-01 13:42:33 +0300

    Schema

---
+++ MKT.Website.UI/Models/PersonSchema.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace MKT.Website.UI.Models
+{
+    public class PersonSchema
+    {
+        [JsonProperty("@context")]
+        public string Context { get; set; } = "https://schema.org";
+
+        [JsonProperty("@type")]
+        public string Type { get; set; } = "WebSite";
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("telephone")]
+        public string Telephone { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+        // Add more properties as needed
+    }
+
+}
